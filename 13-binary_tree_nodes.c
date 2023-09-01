b796fba6807d74e7098986440f47905cd7e1e4c5
astro543 <kinyuab54@gmail.com>
2023-09-01 03:25:23 -0700

    binary trees

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to a node to check
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child
+ * @tree: pointer to the root node
+ *
+ * Return: number of childs of the node
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+	{
+		return (0);
+	}
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+	return (1 + left + right);
+}
